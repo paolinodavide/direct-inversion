f417290dada954ab38a017bbaed2a7c04503091e
paolinodavide <paolino.da01@gmail.com>
2025-05-04 20:34:18 +0200

    MAJOR: updated grinter_parallel from Olivier version -> more readibility

--- grinter_parallel.py
+++ grinter_parallel.py
@@ -1,204 +1,153 @@
 import json
-import sys
 import numpy as np
 from gr_pair import *
 from gr_iteration import *
-import xgboost
 from gr_pair_parallel import *
 
-# Load parameters
-with open("config.json") as f:
-    params = json.load(f)
+def load_config():
+    with open("config.json") as f:
+        return json.load(f)
 
-n_part = params['n_a_part'] + params['n_b_part']
-pi = np.pi
-l_box = params['l_box']
-rho = n_part/l_box**2
-r_bin = params['r_bin']
-qdim = params['qdim_max']
-x_cut = params['x_cut']
-x_min = params['x_min']
-x_low = params['x_low']
-x_max = 10
-binmin = int(x_min/r_bin)
-binlow = int(x_low/r_bin)
-bincut = int(x_cut/r_bin)
-binmax = int(10/r_bin)
-target_file = params['target_file']
-fileout = params['output_file']
-max_iter = params['max_iter']
-method = params['method']
-T = params['Temperature']
-init_pot = params['init_pot']
-delta_reg = params['delta_reg']
-INIT = params['INIT']
-GR = params['GR']
+def load_positions():
+    with open("positions.json") as f:
+        return json.load(f)['wt']
 
-# store the data
-dict_pot = {}
-dict_gr = {}
-dict_err = {}
+def initialize_potential(params, pot_length, r_bin, x_low):
+    if params['INIT']:
+        u = get_pot(params['init_pot'], pot_length, r_bin, x_low, params['Temperature'])
+        x = get_x(u, x_low, r_bin, pot_length)
+    else:
+        data = np.genfromtxt(f"{params['init_pot']}.dat", delimiter='\t', usecols=(0, 1, 2))
+        u = data[:, 1]
+        x = data[:, 2]
+    return u, x
 
-# Initialise waiting times
-with open("positions.json") as f:
-    dict_pos = json.load(f)
-cor_wt = dict_pos['wt']
-n_correl_wt = len(cor_wt)
+def load_target_gr(params, qdim):
+    g_target = np.zeros(qdim)
+    if params['GR'] == 'HISTO':
+        with open(params['target_file'], 'r') as f:
+            lines = [line for line in f if line.strip() and not line.startswith('#')]
+        data = np.array([[float(v) for v in line.strip().split()] for line in lines])
+        g_target[:len(data)] = data[:, 1]
+    return g_target
 
-# Initialise the potential
-pot_length = bincut - binlow
-u_tmp = get_pot('lj_full', pot_length, r_bin, x_low, T)
-x_tmp = init_x(pot_length, r_bin, x_low, T)
-u_tgt = np.zeros(pot_length)
-for j in range(0, pot_length):
-    u_tgt[j] = u_tmp[j]
+def compute_prefactor(n_part, rho, n_correl_wt):
+    return 2. * np.pi * n_part * rho * n_correl_wt / 4.
 
-dict_pot['target'] = []
-for j in range(0, len(u_tmp)):
-    dict_pot['target'].append([x_low + j*r_bin, u_tmp[j], x_tmp[j]]) 
-# Initialize g(r)
-prefactor = 2.*pi*n_part*rho*n_correl_wt/4.
-g_tgt = np.zeros(qdim)
-if (GR == 'FORCE'):
-    gr_tmp = gen_grBorgis_cpp(n_part, qdim, l_box, r_bin, x_max,  \
-            x_cut, binmin, binlow, x_tmp)
-    for wt in cor_wt:
-        for k in range(0, qdim):
-            gr_tmp['tot'][k] += gr_tmp[wt][k]
+def build_dict(entries):
+    return [[x, y, z] for x, y, z in zip(*entries)]
 
-    for i in range(0, binmax):
-        if (method == 'in'):
-            g_tgt[i] = 1. - g_tmp['tot'][i]/prefactor
+def compute_error_metrics(g_target, g_current, u_target, u_current, x_low, x_cut, r_bin):
+    e_gr = get_error(g_target, g_current, x_low, x_cut, r_bin)
+    e_pot = get_error(u_target, u_current, x_low, x_cut, r_bin)
+    return e_gr, e_pot
 
-elif (GR == 'HISTO'):
-    size = 0
-    with open(target_file, 'r') as f:
-        list_target_data = f.read()
-    list_target_lines = list_target_data.split("\n")
-    for line in list_target_lines:
-        if (line.split() != []) and (line.split()[0] != '#'):
-            size += 1
-    list_target = np.zeros((size, 2))
-    i = 0
-    for line in list_target_lines:
-        if (line.split() != []) and (line.split()[0] != '#'):
-            list_target[i] = [float(line.split("\t")[0]), float(line.split("\t")[1])]
-            i += 1
-    for j in range(0, qdim):
-        g_tgt[j] = list_target[j][1]
+def main():
+    params = load_config()
+    cor_wt = load_positions()
 
-# Prepare the error computation
-g_error_tgt = np.zeros(pot_length)
-for i in range(binlow, bincut):
-    g_error_tgt[i - binlow] = g_tgt[i]
-delta_tgt = g_tgt[binlow]
+    n_part = params['n_a_part'] + params['n_b_part']
+    l_box = params['l_box']
+    rho = n_part / l_box**2
+    r_bin = params['r_bin']
+    qdim = params['qdim_max']
+    x_min, x_low, x_cut = params['x_min'], params['x_low'], params['x_cut']
+    binmin, binlow, bincut, binmax = map(lambda x: int(x / r_bin), [x_min, x_low, x_cut, 10])
+    pot_length = bincut - binlow
 
-dict_gr['target'] = []
-for j in range(0, qdim):
-    dict_gr['target'].append([j*r_bin, g_tgt[j]])
+    g_target_full = load_target_gr(params, qdim)
+    g_target = g_target_full[binlow:bincut]
+    delta_tgt = g_target_full[binlow]
 
-# Initialise the potential
-#if (INIT == 'True'):
-if (INIT):
-    u_tmp = get_pot(init_pot, pot_length, r_bin, x_low, T)
-    x_tmp = get_x(u_tmp, x_low, r_bin, pot_length)
-else:
-    # Initialize u on previous data
-    data_u = np.genfromtxt(f'{str(init_pot)}.dat', delimiter = '\t', usecols = (0,1,2))
-    for i in range(0, pot_length):
-        u_tmp[i] = data_u[i][1]
-        x_tmp[i] = data_u[i][2]
+    u_target = get_pot('lj_full', pot_length, r_bin, x_low, params['Temperature'])
+    x_target = init_x(pot_length, r_bin, x_low, params['Temperature'])
 
-dict_pot['0'] = []
-for j in range(0, len(u_tmp)):
-    dict_pot['0'].append([x_low + j*r_bin, u_tmp[j], x_tmp[j]])
+    dict_gr = {'target': [[j * r_bin, g_target_full[j]] for j in range(qdim)]}
+    dict_pot = {'target': [[x_low + j*r_bin, u_target[j], x_target[j]] for j in range(pot_length)]}
+    dict_err = {'gr': [], 'pot': [], 'delta_tgt': [], 'delta_pot': [], 'alpha_pot': []}
 
-precision = params['target_precision']
-iter_index = 1
-final_index = 1
-Dg2 = 1.
+    u_current, x_current = initialize_potential(params, pot_length, r_bin, x_low)
+    dict_pot['0'] = [[x_low + j*r_bin, u_current[j], x_current[j]] for j in range(pot_length)]
 
-dict_err['gr'] = []
-dict_err['pot'] = []
-dict_err['delta_tgt'] = []
-dict_err['delta_pot'] = []
-dict_err['alpha_pot'] = []
+    prefactor = compute_prefactor(n_part, rho, len(cor_wt))
+    precision = params['target_precision']
+    max_iter = params['max_iter']
+    method = params['method']
+    delta_reg = params['delta_reg']
+    T = params['Temperature']
 
-#while (precision > eps):
-while True:
+    best_error = 1.
+    best_index = 1
+    iteration = 1
 
-    if (iter_index == max_iter + 1):
-        print('Error: Target precision is not reached')
-        gr_final = gr_res
-        break
+    while True:
+        if iteration > max_iter:
+            print("Error: Target precision not reached.")
+            break
+        if iteration > 1:
+            g_previous = g_current
 
-    print('Iteration #: ', iter_index, '...')
-    gr_tmp = gen_grBorgis_cpp(n_part, qdim, l_box, r_bin, x_max, \
-            x_cut, binmin, binlow, x_tmp)
-    #for wt in cor_wt:
-    #    for k in range(0, qdim):
-    #        gr_tmp['tot'][k] += gr_tmp[wt][k]
-    delta_pot = 1 - gr_tmp[binlow]/prefactor
+        print(f"Iteration #{iteration} ...")
 
-    gr_res =np.zeros(qdim)
-    for j in range(0, binmax):
-        if (method == 'in'):
-            gr_res[j] = 1. - gr_tmp[j]/prefactor
-            gr_res[j] = abs( ((1. - delta_tgt)*gr_res[j] + (delta_tgt - delta_pot) \
-                    )/(1. - delta_pot) )
-        elif (method == 'out'):
-            gr_res[j] = gr_tmp[j]/prefactor
-            gr_res[j] = gr_res[j]/(delta_pot)
+        gr_raw = gen_grBorgis_cpp(n_part, qdim, l_box, r_bin, 10, x_cut, binmin, binlow, x_current)
+        delta_pot = 1 - gr_raw[binlow] / prefactor
 
-    g_error_tmp = np.zeros(pot_length)
-    for j in range(binlow, bincut):
-        g_error_tmp[j - binlow] = gr_res[j]
-    error1 = get_error(g_error_tgt, g_error_tmp, x_low, x_cut, r_bin)
-    error2 = get_error(u_tgt, u_tmp, x_low, x_cut, r_bin)
+        gr_res = np.zeros(qdim)
+        for j in range(binmax):
+            if method == 'in':
+                temp = 1. - gr_raw[j] / prefactor
+                gr_res[j] = abs(((1. - delta_tgt) * temp + (delta_tgt - delta_pot)) / (1. - delta_pot))
+            elif method == 'out':
+                temp = gr_raw[j] / prefactor
+                gr_res[j] = temp / delta_pot
 
-    dict_err['gr'].append(error1)
-    dict_err['pot'].append(error2)
-    dict_err['delta_tgt'].append(delta_tgt)
-    dict_err['delta_pot'].append(delta_pot)
+        g_current = gr_res[binlow:bincut]
 
-    print('precision: ', error1, 'Error pot:', error2)
-    print('delta_tgt:', delta_tgt, 'delta_pot:', delta_pot)
-    print('Delta alpha_pot : ', (1. - delta_tgt)/(1 - delta_pot) - 1.)
-    if (error1 < Dg2):
-        final_index = iter_index
-        gr_final = gr_res
-        print('best index = ', final_index)
-        Dg2 = error1
+        err_gr = get_error(g_target, g_current, x_low, x_cut, r_bin)
+        err_pot = get_error(u_target, u_current, x_low, x_cut, r_bin)
+        err_iteration = get_error(g_target, g_previous, x_low, x_cut, r_bin) if iteration > 1 else 0
+        
+        dict_err['iteration'] = dict_err.get('iteration', [])
+        dict_err['iteration'].append(err_iteration)
+        dict_err['gr'].append(err_gr)
+        dict_err['pot'].append(err_pot)
+        dict_err['delta_tgt'].append(delta_tgt)
+        dict_err['delta_pot'].append(delta_pot)
 
-    dict_gr[str(iter_index)] = []
-    for j in range(0, qdim):
-        dict_gr[str(iter_index)].append([j*r_bin, gr_res[j]])
+        print(f"Precision: {err_gr:.4e}, Error pot: {err_pot:.4e}")
+        print(f"Delta tgt: {delta_tgt:.4e}, Delta pot: {delta_pot:.4e}")
+        print(f"Delta alpha_pot: {(1. - delta_tgt)/(1 - delta_pot) - 1.:.4e}")
+        print(f"Error iteration: {err_iteration:.4e}")
 
-    alpha_pot = (1. - delta_tgt)/(1 - delta_pot)
-    u_tmp = update_pot(pot_length, delta_reg, T, g_tgt, gr_res, u_tmp, r_bin, \
-            x_low, alpha_pot)
-    x_tmp = get_x(u_tmp, x_low, r_bin, pot_length)
+        if err_gr < best_error:
+            best_error = err_gr
+            best_index = iteration
+            gr_final = gr_res
+            print("Best index =", best_index)
 
-    dict_pot[str(iter_index)] = []
-    for j in range(0, len(u_tmp)):
-        dict_pot[str(iter_index)].append([x_low + j*r_bin, u_tmp[j], x_tmp[j]])
+        dict_gr[str(iteration)] = [[j * r_bin, gr_res[j]] for j in range(qdim)]
 
-    dict_err['alpha_pot'].append(alpha_pot)
+        alpha = (1. - delta_tgt) / (1 - delta_pot)
+        dict_err['alpha_pot'].append(alpha)
 
-    if (error1 < precision):
-        print('Target precision reached at iteration ', iter_index)
-        print('Relative error: ', precision)
-        gr_final = gr_res
-        break
-    
-    iter_index += 1
+        u_current = update_pot(pot_length, delta_reg, T, g_target_full, gr_res, u_current, r_bin, x_low, alpha)
+        x_current = get_x(u_current, x_low, r_bin, pot_length)
 
-#pool.close()
+        dict_pot[str(iteration)] = [[x_low + j*r_bin, u_current[j], x_current[j]] for j in range(pot_length)]
 
-print('Best index: ', final_index)
-dict_gr['best'] = final_index
+        if err_gr < precision:
+            print(f"Target precision reached at iteration {iteration}")
+            break
 
-print('... Saving results ...')
-json.dump(dict_gr, open('gr.json', 'w+'))
-json.dump(dict_pot, open('pot.json', 'w+'))
-json.dump(dict_err, open('err.json', 'w+'))
+        iteration += 1
+
+    print("Best index:", best_index)
+    dict_gr['best'] = best_index
+
+    print("... Saving results ...")
+    json.dump(dict_gr, open('gr.json', 'w+'))
+    json.dump(dict_pot, open('pot.json', 'w+'))
+    json.dump(dict_err, open('err.json', 'w+'))
+
+if __name__ == "__main__":
+    main()
