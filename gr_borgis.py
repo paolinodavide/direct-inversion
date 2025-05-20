68be39834da2b74d54921ed805c29350618290b3
paolinodavide <paolino.da01@gmail.com>
2025-05-20 15:34:59 +0200

    MAJOR: gr_borgis.py correction to pbc

--- gr_borgis.py
+++ gr_borgis.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 from functools import partial
 from numba import njit
+from tqdm import tqdm
 from cProfile import Profile
 import pstats
 import shutil
 
-import gr_iteration as it
+import forceIBI.gr_iteration as it
 
 global NUM_THREADS
 NUM_THREADS = 12
@@ -69,7 +70,16 @@ def gBorgis_numpy(file, box_size, binned_force, bins):
 
     return np.cumsum(gr_contributions)
 
-@njit(fastmath=False) #Clear the cache if you want to recompile
+@njit
+def periodic_boundary_conditions(dx, box_length):
+    """Fastest PBC implementation using floor division."""
+    half_box = 0.5 * box_length
+    if dx >= 0:
+        return dx - box_length * np.floor((dx + half_box) / box_length)
+    else:
+        return dx - box_length * np.ceil((dx - half_box) / box_length)
+
+@njit
 def grBorgis_notNorm(particle_positions, box_length, min_radius, r_bin, num_bins, force_div_r, rlow, r_cut, method='out'):
     """Compute the Borgis g(r) using Numba for a given configuration file. The normalization is not included."""
     num_particles, dimensions = particle_positions.shape
@@ -82,9 +92,9 @@ def grBorgis_notNorm(particle_positions, box_length, min_radius, r_bin, num_bins
         for j in range(i+1, num_particles):
             if i != j:
                 dx_ij = particle_positions[i, 0] - particle_positions[j, 0]
-                dx_ij -= box_length * np.round(dx_ij / box_length)
+                dx_ij = periodic_boundary_conditions(dx_ij, box_length)
                 dy_ij = particle_positions[i, 1] - particle_positions[j, 1]
-                dy_ij -= box_length * np.round(dy_ij / box_length)
+                dy_ij = periodic_boundary_conditions(dy_ij, box_length)
 
                 d_ij_squared = dx_ij * dx_ij + dy_ij * dy_ij
                 if d_ij_squared == 0.0 or d_ij_squared > r_cut * r_cut:  # Skip self-interaction and cut-off
@@ -95,7 +105,7 @@ def grBorgis_notNorm(particle_positions, box_length, min_radius, r_bin, num_bins
                 if binIdx_ij > binlow: #Already below the cut-off
                     alpha = d_ij / r_bin - binIdx_ij
                     force_magnitude = alpha * force_div_r[binIdx_ij - binlow] + (1 - alpha) * force_div_r[binIdx_ij - binlow + 1]
-                elif binIdx_ij <= binlow:
+                elif binIdx_ij <=  binlow:
                     alpha = binlow - binIdx_ij + 1
                     force_magnitude = force_div_r[0] + alpha * (force_div_r[0] - force_div_r[1])
                 else:
@@ -105,33 +115,29 @@ def grBorgis_notNorm(particle_positions, box_length, min_radius, r_bin, num_bins
                 net_forces[i, 1] += dy_ij * force_magnitude
                 net_forces[j, 0] -= dx_ij * force_magnitude  # Newton's 3rd law
                 net_forces[j, 1] -= dy_ij * force_magnitude
-
-    
+   
     # 2) Compute Borgis Delta contributions
     for i in range(num_particles):
         for j in range(i + 1, num_particles):
             dx_ij = particle_positions[i, 0] - particle_positions[j, 0]
-            dx_ij -= box_length * np.round(dx_ij / box_length)
+            dx_ij = periodic_boundary_conditions(dx_ij, box_length)
             dy_ij = particle_positions[i, 1] - particle_positions[j, 1]
-            dy_ij -= box_length * np.round(dy_ij / box_length)
-
+            dy_ij = periodic_boundary_conditions(dy_ij, box_length)
             d_ij_squared = dx_ij * dx_ij + dy_ij * dy_ij
-            if d_ij_squared == 0.0:
-                continue
-            d_ij = np.sqrt(d_ij_squared)
+            
+            binIndex_ij = int( np.sqrt(d_ij_squared) / r_bin) 
 
-            binIndex_ij = int((d_ij) / r_bin)
-            if binIndex_ij < 0:
-                continue
-            if binIndex_ij >= num_bins:
-                binIndex_ij = num_bins - 1 if method == 'out' else num_bins 
-                
             # Delta_ij = ((Fi − Fj) ⋅ rij) / r²
             force_diff_x = net_forces[i, 0] - net_forces[j, 0]
             force_diff_y = net_forces[i, 1] - net_forces[j, 1]
-            dot_product = force_diff_x * dx_ij + force_diff_y * dy_ij
-            borgis_contributions[binIndex_ij] += dot_product / d_ij_squared
-
+            Delta_ij = (force_diff_x * dx_ij + force_diff_y * dy_ij) / d_ij_squared
+        
+            if binIndex_ij >= num_bins:
+                #binIndex_ij = num_bins - 1 if method == 'out' else num_bins 
+                binIndex_ij = int(r_cut / r_bin) - 1
+            
+            borgis_contributions[binIndex_ij] += Delta_ij 
+    
     # 3) Cumulative sum of contributions
     if method == 'in':
         g0 = np.cumsum(borgis_contributions)
@@ -203,39 +209,76 @@ def main():
     max_radius = 10.0
     x_cut = 2.5
     method = 'out'
-    rlow = 0.99
-    pot_length = int((x_cut - rlow) / bin_width)
+    x_low = 0.99
+    pot_length = int((x_cut - x_low) / bin_width)
+
+    ordered_indices_file = "../ordered_wt.dat"
+    configs_path = "../configs_npy/lj"
+    max_config_nb = -1
+
 
     # Load the binned force data (example)
-    potential = it.get_pot('lj_full', pot_length, bin_width, rlow, 1.0)
-    force_div_r = it.get_x(potential, rlow, bin_width, pot_length)
-    prefactor = 2 *np.pi * 2025**2 / box_length**2 /4
+    potential = it.get_pot('lj_full', pot_length, bin_width, x_low, 1.0)
+    force_div_r = it.get_x(potential, x_low, bin_width, pot_length)
 
-    profiler = Profile()
-    profiler.enable()
-    
-    # Compute gBorgis
-    gr_inf = 1 - gBorgis_parallel(box_length, force_div_r, rlow, max_radius, bin_width, x_cut, method='out')/prefactor
-    gr_zero = gBorgis_parallel(box_length, force_div_r, rlow, max_radius, bin_width, x_cut, method='in') / prefactor
 
-    profiler.disable()
-    stats = pstats.Stats(profiler)
-    stats.sort_stats(pstats.SortKey.CUMULATIVE)
-    with open("profiling.log", "w") as log_file:
-        stats.stream = log_file
-        stats.print_stats()
+    # Convert force_div_r to a numpy array
+    x_current = np.asarray(force_div_r)
+
+    # Load ordered indices from file
+    ordered_indices = np.loadtxt(ordered_indices_file, dtype=int)
+    ordered_indices = ordered_indices[:max_config_nb]
+
+    # Generate list of configuration files
+    config_files = [
+        f"{configs_path}_{i}.npy"
+        for i in ordered_indices
+        if os.path.exists(f"{configs_path}_{i}.npy")
+    ]
+    print(f"Evaluating Borgis g(r) for {len(config_files)} files...")
+
+    pot_length = int((max_radius - min_radius) / bin_width)
+
+    # Warm up compilation with the first configuration file
+    _ = cmpt_borgis_from_file(config_files[0], box_length, min_radius, bin_width, pot_length, x_current, x_low, x_cut, method)
+
     
-    # Plot gr_inf and gr_zero
-    r_values = np.arange(min_radius, max_radius, bin_width)
-    plt.figure(figsize=(10, 6))
-    plt.plot(r_values, gr_inf, 'o-', label="g(r) - out", color="blue")
-    plt.plot(r_values, gr_zero, 'o-', label="g(r) - in", color="red")
-    plt.xlabel("r")
-    plt.ylabel("g(r)")
-    plt.title("Borgis g(r)")
-    plt.legend()
+    config = np.load(config_files[0])
+    N, dimensions = config.shape
+    prefactor = 2 *np.pi * N**2 / box_length**2 /4
+
+
+    # Parallel computation using multiprocessing
+    with Pool(NUM_THREADS) as pool:
+        borgis_results = list(
+            tqdm(
+                pool.imap(partial(cmpt_borgis_from_file,
+                        box_length=box_length,
+                        min_radius=min_radius,
+                        bin_width=bin_width,
+                        num_bins=pot_length,
+                        force_bins=x_current,
+                        rlow=x_low,
+                        r_cut=x_cut,
+                        method=method),
+                    config_files),
+                total=len(config_files),
+                desc="Processing configurations"
+            )
+        )
+
+    # Compute and return the mean g(r)
+    if method == 'out':
+        gr = 1 - np.mean(borgis_results, axis=0)/prefactor
+    if method == 'in':
+        gr = np.mean(borgis_results, axis=0)/prefactor
+
+    plt.plot( gr, label='g(r) Borgis')
+    plt.xlabel('Distance (r)')
+    plt.ylabel('g(r)')
+    plt.title(f'Borgis g(r) from method {method}')
     plt.grid()
-    plt.savefig("gr_plot.png")
+    plt.legend()
     plt.show()
     return
 
