45d6a72dab6c16441ad18ff0199a5179fcdb96a1
paolinodavide <paolino.da01@gmail.com>
2026-02-03 15:07:25 +0100

    fix: solved update metrics with negative gr

--- grinter_parallel.jl
+++ grinter_parallel.jl
@@ -159,7 +159,8 @@ end
 function compute_convergence_metrics(gr_current, gr_target, gr_old)
     error = sum((gr_current .- gr_target).^2) / length(gr_target)
     iteration_diff = sum((gr_current .- gr_old).^2) / length(gr_current)
-    potential_increase = sum((log.(gr_current ./ gr_target)).^2) / length(gr_current)
+    gr_temp = gr_current .- minimum(gr_current) .+ minimum(gr_target) 
+    potential_increase = sum((log.(gr_temp ./ gr_target)).^2) / length(gr_current)
     return error, iteration_diff, potential_increase
 end
 
